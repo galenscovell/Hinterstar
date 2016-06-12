45d362e73f4bae3745e5509fea5717e26540967f
Galen <galen.scovell@gmail.com>
2016-06-11 19:00:54 -0700

    Part discovery and update on PartSelectionPanel

--- core/src/galenscovell/hinterstar/ui/components/startscreen/PartSelectionPanel.scala
+++ core/src/galenscovell/hinterstar/ui/components/startscreen/PartSelectionPanel.scala
@@ -5,14 +5,17 @@ import com.badlogic.gdx.scenes.scene2d.ui.{Label, Table, TextButton}
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.Align
 import galenscovell.hinterstar.util.{Constants, ResourceManager}
+import galenscovell.hinterstar.things.parts.{Part, PartParser}
 
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ArrayBuffer, Map}
 
 
 class PartSelectionPanel extends Table {
   private val moneyLabel: Label = new Label("$100,000.00", ResourceManager.labelMenuStyle)
   moneyLabel.setAlignment(Align.center, Align.center)
   private var currentPartType: TextButton = null
+  private val partTypes: List[String] = List("Combat", "Engine", "Hull", "Power", "Shield", "Storage")
+  private val partMap: Map[String, Array[Part]] = createPartMap
 
   construct()
 
@@ -54,8 +57,6 @@ class PartSelectionPanel extends Table {
 
     val typeButtonsTable: Table = new Table
 
-    val partTypes: List[String] = List("Combat", "Engine", "Hull", "Power", "Shield", "Storage")
-
     for (x <- partTypes.indices) {
       val typeButton: TextButton = new TextButton(partTypes(x), ResourceManager.toggleButtonStyle)
       typeButton.addListener(new ClickListener() {
@@ -67,6 +68,7 @@ class PartSelectionPanel extends Table {
             currentPartType = typeButton
           }
           currentPartType.setChecked(true)
+          displayParts(typeButton.getText.toString)
         }
       })
       typeButtonsTable.add(typeButton).width(102).height(50).pad(5)
@@ -80,5 +82,24 @@ class PartSelectionPanel extends Table {
     bottomTable.add(moneyTable).expand.pad(2)
 
     bottomTable
+  }
+
+  private def createPartMap: Map[String, Array[Part]] = {
+    val partParser: PartParser = new PartParser
+    val map: Map[String, Array[Part]] = Map()
+
+    for (partType: String <- partTypes) {
+      val parts: Array[Part] = partParser.parseAll(partType, "rank-1")
+      map.put(partType, parts)
+    }
+    map
+  }
+
+  private def displayParts(partType: String): Unit = {
+    val parts: Array[Part] = partMap.get(partType).get
+
+    for (p: Part <- parts) {
+      println(p.getName)
+    }
   }
 }
