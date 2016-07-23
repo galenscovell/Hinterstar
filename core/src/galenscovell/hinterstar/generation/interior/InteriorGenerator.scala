998f817d8bc6c3e1507599643864e406a2d098ed
Galen <galen.scovell@gmail.com>
2016-07-23 11:37:22 -0700

    Implement room connections for BSP

--- core/src/galenscovell/hinterstar/generation/interior/InteriorGenerator.scala
+++ core/src/galenscovell/hinterstar/generation/interior/InteriorGenerator.scala
@@ -16,6 +16,8 @@ class InteriorGenerator(width: Int, height: Int, numberOfSplits: Int) {
   build()
   partition(numberOfSplits)
   debugPrint()
+  connectRooms(partitions(0))
+  debugPrint()
 
 
   private def build(): Unit = {
@@ -30,11 +32,12 @@ class InteriorGenerator(width: Int, height: Int, numberOfSplits: Int) {
   private def partition(splits: Int): Unit = {
     // Binary space partitioning
     val random: Random = new Random()
-    val rootRoom: Room = new Room(0, 0, columns, rows)
+    val rootRoom: Room = new Room(0, 0, columns, rows, null)
     partitions.append(rootRoom)
 
+    var attempts: Int = 300
     var splitAmount: Int = 0
-    while (splitAmount < desiredSplits) {
+    while (splitAmount < desiredSplits && attempts > 0) {
       val chosenPartition: Int = random.nextInt(partitions.size)
       val toSplit: Room = partitions(chosenPartition)
       if (toSplit.split()) {
@@ -42,6 +45,7 @@ class InteriorGenerator(width: Int, height: Int, numberOfSplits: Int) {
         partitions.append(toSplit.rightChild)
         splitAmount += 1
       }
+      attempts -= 1
     }
     generateInterior(rootRoom)
   }
@@ -75,11 +79,47 @@ class InteriorGenerator(width: Int, height: Int, numberOfSplits: Int) {
     room.setTiles(roomTiles.toArray)
   }
 
+  private def connectRooms(rootRoom: Room): Unit = {
+    // Recursively connect each rooms children to each other with hallways
+    if (rootRoom != null) {
+      if (rootRoom.leftChild != null) {
+        connectRooms(rootRoom.leftChild)
+      }
+      if (rootRoom.rightChild != null) {
+        connectRooms(rootRoom.rightChild)
+      }
+    }
+
+    if (rootRoom.sister != null && !rootRoom.isConnected()) {
+      createHall(rootRoom, rootRoom.sister)
+    }
+  }
+
+  private def createHall(r1: Room, r2: Room): Unit = {
+    // Create hall between centerpoints of a pair of rooms
+    val xDelta: Int = r1.centerX - r2.centerX
+    val yDelta: Int = r1.centerY - r2.centerY
+
+    if (xDelta == 0) {
+      for (y <- r1.centerY to r2.centerY) {
+        tiles(r1.centerX)(y).becomeFloor()
+      }
+    } else if (yDelta == 0) {
+      for (x <- r1.centerX to r2.centerX) {
+        tiles(x)(r1.centerY).becomeFloor()
+      }
+    }
+
+    r1.setConnected()
+    r2.setConnected()
+  }
+
   private def isOutOfBounds(x: Int, y: Int): Boolean = {
     x < 0 || y < 0 || x >= columns || y >= rows
   }
 
   private def debugPrint(): Unit = {
+    println()
     for (row: Array[Tile] <- tiles) {
       println()
       for (tile: Tile <- row) {
--- core/src/galenscovell/hinterstar/generation/interior/Room.scala
+++ core/src/galenscovell/hinterstar/generation/interior/Room.scala
@@ -3,12 +3,17 @@ package galenscovell.hinterstar.generation.interior
 import scala.util.Random
 
 
-class Room(topLeftX: Int, topLeftY: Int, w: Int, h: Int) {
+class Room(topLeftX: Int, topLeftY: Int, w: Int, h: Int, p: Room) {
   val x: Int = topLeftX
   val y: Int = topLeftY
-  var width: Int = w
-  var height: Int = h
-  var leftChild, rightChild: Room = _
+  val width: Int = w
+  val height: Int = h
+  val centerX: Int = topLeftX + (width / 2)
+  val centerY: Int = topLeftY + (height / 2)
+
+  val parent: Room = p
+  var sister, leftChild, rightChild: Room = _
+  var connected: Boolean = false
 
   var tiles: Array[Tile] = _
   var subsystem: String = _
@@ -16,6 +21,18 @@ class Room(topLeftX: Int, topLeftY: Int, w: Int, h: Int) {
 
   def setTiles(tiles: Array[Tile]): Unit = {
     this.tiles = tiles
+  }
+
+  def setSister(r: Room): Unit = {
+    sister = r
+  }
+
+  def isConnected(): Boolean = {
+    connected
+  }
+
+  def setConnected(): Unit = {
+    connected = true
   }
 
   def setSubsystem(subsystem: String): Unit = {
@@ -66,15 +83,18 @@ class Room(topLeftX: Int, topLeftY: Int, w: Int, h: Int) {
 
     if (horizontal) {
       // Top horizontal child
-      leftChild = new Room(x, y, width, split)
+      leftChild = new Room(x, y, width, split, this)
       // Bottom horizontal child
-      rightChild = new Room(x, y + split, width, height - split)
+      rightChild = new Room(x, y + split, width, height - split, this)
     } else {
       // Left vertical child
-      leftChild = new Room(x, y, split, height)
+      leftChild = new Room(x, y, split, height, this)
       // Right vertical child
-      rightChild = new Room(x + split, y, width - split, height)
+      rightChild = new Room(x + split, y, width - split, height, this)
     }
+
+    leftChild.setSister(rightChild)
+    rightChild.setSister(leftChild)
 
     // Split successful
     true
