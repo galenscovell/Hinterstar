a66df174bcd78a6ad514bb640ba68854676d074f
Galen <galen.scovell@gmail.com>
2016-08-26 21:00:56 -0700

    Begin differentiating weaponFX types

--- core/src/galenscovell/hinterstar/graphics/WeaponFx.scala
+++ core/src/galenscovell/hinterstar/graphics/WeaponFx.scala
@@ -10,11 +10,13 @@ import galenscovell.hinterstar.util.Resources
   * Houses sprite, source coordinates, target coordinates, velocity, etc.
   */
 class WeaponFx(fxType: String, speed: Float) {
+  private val fxClass: String = fxType
   private val sprite: Sprite = new Sprite(Resources.atlas.createSprite(fxType))
   private var start: Vector2 = _
   private var pos: Vector2 = _
   private var destination: Vector2 = _
   private val velocity: Float = speed
+  private var frames: Int = 60
 
 
 
@@ -22,21 +24,33 @@ class WeaponFx(fxType: String, speed: Float) {
     start = src
     pos = start.cpy()
     destination = dest
+    frames = 60
   }
 
   def done: Boolean = {
-    // Return true if current position surpasses destination
-    val diffX: Float = Math.abs(destination.x - pos.x)
-    val diffY: Float = Math.abs(destination.y - pos.y)
-    diffX <= 4 && diffY <= 8
+    // Return true if current position surpasses destination or frames == 0
+    fxClass match {
+      case "fx_railgun" =>
+        val diffX: Float = Math.abs(destination.x - pos.x)
+        val diffY: Float = Math.abs(destination.y - pos.y)
+        diffX <= 4 && diffY <= 4
+      case "fx_basic_laser" =>
+        frames -= 1
+        frames <= 0
+    }
   }
 
   def draw(delta: Float, spriteBatch: Batch): Unit = {
-    pos.x += (destination.x - start.x) * velocity * delta
-    pos.y += (destination.y - start.y) * velocity * delta
-    spriteBatch.draw(sprite, pos.x, pos.y, 8, 8)
+    fxClass match {
+      case "fx_railgun" =>
+        pos.x += (destination.x - start.x) * velocity * delta
+        pos.y += (destination.y - start.y) * velocity * delta
+        spriteBatch.draw(sprite, pos.x, pos.y, 4, 4)
 
-    spriteBatch.draw(sprite, start.x, start.y, 8, 8)
-    spriteBatch.draw(sprite, destination.x, destination.y, 8, 8)
+      case "fx_basic_laser" =>
+        val distance: Float = start.dst(destination)
+        val degrees: Float = Math.toDegrees(Math.atan2(destination.y - start.y, destination.x - start.x)).toFloat
+        spriteBatch.draw(sprite, start.x, start.y, 0, 0, distance, 4, 1, 1, degrees)
+    }
   }
 }
